6328273bac536fdbb5cefed95911aa49d4545a1c
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2008-04-18 07:40:46 +0000

    added module list_types
    git-svn-id: https://afrodite.eln.uniroma2.it/svn/uptight/trunk@5 9f5beb75-5649-0410-8c9a-ed96348d3a86

--- src/lists.f90
+++ src/lists.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e60cab49042829954de3b093384244533a78fb76f8fcab64c682e686ad33c351
-size 29855
+oid sha256:6a6dce4a5a6db2e15ce5038eb736e16c8533e5227ae459dcdb336ceeca20f769
+size 29884
---
+++ src/listtypes.f90
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3c4bac778bb6e862fe7363e8e052963d744f409f1eb2af5e994221d81b4debf
+size 1929
