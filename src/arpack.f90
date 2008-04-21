8412b6c064b13fca9d577f5d131efd2aea91ffe9
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2008-04-21 14:42:20 +0000

    cured bug due to sparse_fmt in calls to sprs_ax
    git-svn-id: https://afrodite.eln.uniroma2.it/svn/uptight/trunk@15 9f5beb75-5649-0410-8c9a-ed96348d3a86

--- src/arpack.f90
+++ src/arpack.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4da078401f9eddd6fcfa100d0c68d3b67ea387063917b088c9ca816ce3e7987c
-size 11552
+oid sha256:95ff9190eca345ebfc17d7016da5c308611c3394b1b6c00753f04024ac06f8db
+size 11596
--- src/field_effects.f90
+++ src/field_effects.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afaa3a3266dd4f9242b9b6917356f65bba749e2dbdbbe99b876cee79063a803d
-size 14317
+oid sha256:dc1a94f0b8670cb2cba65944f7853d3205283195e5f577add16204e56f4591cd
+size 14329
