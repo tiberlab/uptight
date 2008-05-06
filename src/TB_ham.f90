20d19194d27b130a7cf8c2bd6159cc5fd5782811
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2008-05-06 08:49:36 +0000

    Corrected non-Hermeticity also in optical matrix
    git-svn-id: https://afrodite.eln.uniroma2.it/svn/uptight/trunk@28 9f5beb75-5649-0410-8c9a-ed96348d3a86

--- src/TB_ham.f90
+++ src/TB_ham.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a31ae9d98139a808637beec893d5e8fd40c6623593697c0d3c4ea72aaefe348f
+oid sha256:0fb9d1d6ff71b6097b39cf99e02767d335cfa9cf86e1cb585f57944a2474f63f
 size 163564
