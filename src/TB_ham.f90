1d32c28cc7f5a8b2dcd9a20c143eec74d0bc224b
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2008-05-06 08:43:21 +0000

    corrected non-Hermeticity in dangling bonds and d-rotations
    git-svn-id: https://afrodite.eln.uniroma2.it/svn/uptight/trunk@27 9f5beb75-5649-0410-8c9a-ed96348d3a86

--- src/TB_ham.f90
+++ src/TB_ham.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a0f40237eb8716401d56581acdd6520380cdc607a15132a662ee9ae89ab82df
-size 162675
+oid sha256:a31ae9d98139a808637beec893d5e8fd40c6623593697c0d3c4ea72aaefe348f
+size 163564
