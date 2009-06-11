3bdfece904f14d3182b78cfec304469d60d5a5d3
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2009-06-11 15:54:06 +0000

    added a super_cell_flag check for miller indeces
    git-svn-id: https://afrodite.eln.uniroma2.it/svn/uptight/trunk@87 9f5beb75-5649-0410-8c9a-ed96348d3a86

--- src/supercell.f90
+++ src/supercell.f90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd93391d035d6cc15e26acd4a785bea8a03af90949e98c6352f622793340e665
-size 87806
+oid sha256:fc22f7d66c0ad729d4c7d1cd3da77455127253e3068a19c04c35f070bc8382e8
+size 87815
