4021e43e28387aa3614a4194431ef9260472cd5e
Matthias Auf der Maur <maufder@sirius>
2025-02-02 14:28:13 +0100

    Added Credits and LICENSE (LGPL v3). Added one fortran source

---
+++ Credits.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:92c2160cd2a769c9f72ea24721f2367f75dfe717482ec3ee6deda3765e22e908
+size 210
--- src/out_prop.f90
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:92a06302dbcb509bf4faf37a1c526cb07593fd410d96f685f81aa6c835e6df28
-size 4623
