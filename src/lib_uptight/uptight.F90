2460de43cdda6b43ee9140cb7487bc693cc5cdcc
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2025-03-14 00:37:29 +0100

    use now git for revision number

--- src/lib_uptight/uptight.F90
+++ src/lib_uptight/uptight.F90
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eb5f58d1ebb11302927bdc4cfe0bff0336c9d48f7e7e8057c14aedafaa45c1a
-size 17519
+oid sha256:7577fca13b4f2ad1fb115a50bebb79dbe66dd3f464db0711e40e9bb55398cd85
+size 17514
